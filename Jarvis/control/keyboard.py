110194579b190467aa7f78a03d58cddbf8e47589
Gorillaz <dk2077077@gmail.com>
2026-06-27 21:38:24 +0300

    jarvis v2

--- Jarvis/control/keyboard.py
+++
@@ -1,4 +0,0 @@
-import pyautogui
-
-def type_text(text):
-    pyautogui.write(text, interval=0.03)
--- Jarvis/vision/screen.py
+++
@@ -1,6 +0,0 @@
-import pyautogui
-
-def take_screenshot():
-    img = pyautogui.screenshot()
-    img.save("screen.png")
-    return "screen.png"
